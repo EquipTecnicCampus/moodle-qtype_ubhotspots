af8a8bd1907159b357d51bfaa4bfd895482f9743
jleyva <juanleyvadelgado@gmail.com>
2011-10-17 11:27:04 +0200

    Updated version.php

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
     $plugin->version  = 2011100603;
     $plugin->requires = 2007101000;
-    //$plugin->maturity = MATURITY_STABLE;
-    //$plugin->release = 1.0;
+    $plugin->maturity = MATURITY_STABLE;
+    $plugin->release = 1.0;
 
 ?>
