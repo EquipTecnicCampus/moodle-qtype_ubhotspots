4e53158acc395129c3b5437a7328455d012cc6d0
jleyva <juanleyvadelgado@gmail.com>
2011-11-10 14:23:39 +0100

    Adding missing lang files

---
+++ lang/ca_utf8/qtype_ubhotspots.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The language strings for the ubhotspots question type.
+ *    
+ * @copyright &copy; 2011 Universitat de Barcelona
+ * @author <jleyva@cvaconsulting.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package ubhotspots
+ *
+ */
+
+$string['editingubhotspots'] = 'Editant UB Hot Spots';
+$string['addingubhotspots'] = 'Afegint UB Hot Spots';
+$string['ubhotspots'] = 'UB Hot Spots';
+$string['openeditor'] = 'Obre editor de Hot Spots';
+$string['imagealert'] = 'Primer heu de pujar una imatge per editar-la';
+$string['chooseanimage'] = 'Cal escollir una imatge en el desplegable Imatge que es visualitzarà';
+$string['failedloadinganswers'] = "Error fatal carregant respostes";
+$string['missingimage'] = 'Falta la imagte principal. La pregunta no es visualitzarà';
+
+// HS Editor Javascript strings
+
+$string['jsadd'] = 'Afegeix';
+$string['jssave'] = 'Desa';
+$string['jscancel'] = 'Cancel·la';
+$string['jshotspot'] = 'Hotspot';
+$string['jsdelete'] = 'Esborra forma';
+$string['jsentertext'] = 'Entreu el text...';
+$string['jsellipse'] = 'El·lipse';
+$string['jsrectangle'] = 'Rectangle';
+$string['jsok'] = 'Sí';
+$string['jsno'] = 'No';
+$string['jsclosewithout'] = 'Tancar la finestra sense desar els canvis?';
+$string['jswarning'] = 'avís';
+$string['jsconfirmdelete'] = 'Esteu segur de voler esborrar aquest hotspott?';
+
+?>
---
+++ lang/es_utf8/qtype_ubhotspots.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The language strings for the ubhotspots question type.
+ *    
+ * @copyright &copy; 2011 Universitat de Barcelona
+ * @author <jleyva@cvaconsulting.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package ubhotspots
+ *
+ */
+
+$string['editingubhotspots'] = 'Editando UB Hot Spots';
+$string['addingubhotspots'] = 'Añadiendo UB Hot Spots';
+$string['ubhotspots'] = 'UB Hot Spots';
+$string['openeditor'] = 'Abre el editor de Hot Spots';
+$string['imagealert'] = 'Primero ha de subir una imagen para editarla';
+$string['chooseanimage'] = 'Debe elegir una imagen en el desplegable de imágenes a mostrar';
+$string['failedloadinganswers'] = "Error fatal cargando respuestas";
+$string['missingimage'] = 'Falta la imagen principal. La pregunta no se mostrará';
+
+// HS Editor Javascript strings
+
+$string['jsadd'] = 'Añadir';
+$string['jssave'] = 'Guardar';
+$string['jscancel'] = 'Cancelar';
+$string['jshotspot'] = 'Hotspot';
+$string['jsdelete'] = 'Borrar forma';
+$string['jsentertext'] = 'Entre el texto...';
+$string['jsellipse'] = 'Elipse';
+$string['jsrectangle'] = 'Rectangulo';
+$string['jsok'] = 'Sí';
+$string['jsno'] = 'No';
+$string['jsclosewithout'] = '¿Cerrar la ventana sin guardar los cambios?';
+$string['jswarning'] = 'aviso';
+$string['jsconfirmdelete'] = '¿Está seguro de querer borrar este hotspot?';
+
+?>
