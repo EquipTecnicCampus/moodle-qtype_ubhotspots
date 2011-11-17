4dd55ad25dd2e08eff7f80239190edf083ffc051
Yolanda Ordóñez Rufat <tehanuyor@gmail.com>
2011-11-17 14:52:29 +0100

    Update version.php

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 
     $plugin->version  = 2011100603;
-    $plugin->requires = 2007101000;
+    $plugin->requires = 2007101540;
     $plugin->maturity = MATURITY_STABLE;
     $plugin->release = 1.0;
 
